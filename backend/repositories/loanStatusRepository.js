9099bb997e0232765ceff828be18e87c68a212af
Youssof <youssofelhusseiny10@gmail.com>
2026-01-21 12:11:45 +0200

    Implemented caching for statuses and modified maximum loan amount to 20000 and added a constraint for a user to have one loan request as a maximum

--- backend/repositories/loanStatusRepository.js
+++ backend/repositories/loanStatusRepository.js
@@ -1,8 +1,19 @@
 const LoanStatus = require('../models/LoanStatus');
+const cache = require('../utils/cache');
 
 class LoanStatusRepository {
     async getStatusByName(statusName) {
-        return await LoanStatus.findOne({ name: statusName });
+        const cachedStatus = cache.get(statusName);
+        if (cachedStatus) {
+            console.log(`--- Cache Hit: ${statusName} found in memory ---`);
+            return cachedStatus;
+        }
+        const status = await LoanStatus.findOne({ name: statusName });
+        if (status) {
+            cache.set(statusName, status);
+            console.log(`--- Cache Set: ${statusName} stored in memory ---`);
+        }
+        return status;
     }
 }
 
